d5f8416e2cd3f6fbb97c8b243f981dc920ef2823
Wiem Rukmihadi <wiemrukmihadi@jukesolutions.com>
2025-03-04 09:04:18 +0700

    update

--- models/partner.py
+++ models/partner.py
@@ -6,12 +6,20 @@ from odoo import models, fields, api
 class ResPartner(models.Model):
     _inherit = 'res.partner'
 
-    approver_id = fields.Many2one('res.users', string='Approved By')
+    approver_id = fields.Many2one('res.users', string='Approved By', required=False)
     approval_status = fields.Selection([
         ('draft', 'Draft'),
         ('approved', 'Approved'),
         ('rejected', 'Rejected')
     ], default='draft', string="Approval Status")
+
+    is_admin = fields.Boolean(compute="_compute_is_admin")
+
+    @api.depends('approval_status')
+    def _compute_is_admin(self):
+        """Check if the current user is an admin."""
+        for rec in self:
+            rec.is_admin = self.env.user.has_group('training_contacts.contact_approval_admin')
 
     def action_approve(self):
         for rec in self:
@@ -23,6 +31,7 @@ class ResPartner(models.Model):
     def action_reset(self):
         for rec in self:
             rec.approval_status = 'draft'
+            rec.approver_id = False
 
     # name = fields.Char()
     # value = fields.Integer()
